1f7e677d5b2ad628c5fa4a47d125bbab66de25c5
justin-hill426 <jrhill426@gmail.com>
2022-01-31 18:17:14 -0700

    Created animations and introductory components

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 
 const Header = ({ text }) => {
   return (
-  
       <header style={{ backgroundColor: 'blue'}}>
         <div className='container'>
           <h1>{text}</h1>
